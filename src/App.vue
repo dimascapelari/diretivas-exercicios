f6a7495d3c2b7b9651f6d6c00e1e9137e7bcc9ad
Dimas Capelari <dimas.capelari@gmail.com>
2022-08-17 14:27:59 -0300

    aula 190

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@
     <!-- <p v-destaque>Usando diretiva personalizada</p> -->
     <p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
     <p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
+
+    <hr />
+    <p v-destaque-local:fundo.atrasar="'lightblue'">
+      Usando diretiva personalizada
+    </p>
+    <p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
     <!--   exemplo
     <p v-teste:argumento.mod1.mod2.mod3="'valor'"></p>
 	-->
@@ -16,6 +22,24 @@
 
 <script>
 export default {
+  directives: {
+    "destaque-local": {
+      bind(el, binding, vnode) {
+        // el.style.backgroundColor = 'lightgreen'
+
+        let atraso = 0;
+        if (binding.modifiers["atrasar"]) atraso = 3000;
+
+        setTimeout(() => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, atraso);
+      },
+    },
+  },
   data() {
     return {
       cor: "red",
